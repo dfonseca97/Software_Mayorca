3ad71ee6af2771674ca7dd392777c718ec4205f3
Diego Fonseca <dfonseca@eafit.edu.co>
2018-02-07 11:47:21 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :associates
  	get 'welcome/index'
 
-	resources :associate
-
 	root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
