0bd2360692a93a967dda634c24b7782825538e70
Diego Fonseca <dfonseca@eafit.edu.co>
2018-02-07 13:57:04 -0500

    comentario

--- db/migrate/20180207165945_add_constraints.rb
+++ db/migrate/20180207165945_add_constraints.rb
@@ -3,7 +3,7 @@ class AddConstraints < ActiveRecord::Migration[5.1]
 
   	change_column :associates, :name, :string, null: false
   	change_column :associates, :last_name, :string, null: false
-  	add_index :associates, :code, unique: true
+  	#add_index :associates, :code, unique: true
 
   end
 end
